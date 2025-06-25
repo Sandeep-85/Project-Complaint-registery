6ceb2fc54a312e2f606683d71a9f16fc0b4b5529
sidduwrwt <siddarthaporal@gmail.com>
2025-06-25 13:11:16 +0530

    Delete Project Files/Backend/config.js

--- Project Files/Backend/config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb+srv://siddarthaporal:Siddu2425poral@cluster0.lrn7ndl.mongodb.net/details?retryWrites=true&w=majority&appName=Cluster0")
-.then(() => {
-  console.log("Connected to MongoDB Atlas");
-})
-.catch(err => {
-  console.error("Connection error:", err);
-});
